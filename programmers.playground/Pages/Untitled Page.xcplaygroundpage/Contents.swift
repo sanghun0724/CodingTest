70ce15836d55b67e076e4c7a61802da716b33170
sangheonlee <sanghun0724@naver.com>
2020-10-20 15:07:04 +0900

    good

--- programmers.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ programmers.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -3,5 +3,19 @@
 import Foundation
 
 func solution(_ s:String) -> String {
-    
+    var middleIndex:[Int] {
+        get {
+            if s.count % 2 == 0 {
+                return [s.count / 2 - 1,s.count / 2 ]
+            }
+            return [Int(Double(s.count/2)+0.5)]
+        }
+        
+    }
+    if middleIndex.count == 1 {
+    return "\(s[s.index(s.startIndex, offsetBy: middleIndex[0])])"
+    }
+    return "\(s[s.index(s.startIndex, offsetBy: middleIndex[0])])" + "\(s[s.index(s.startIndex, offsetBy: middleIndex[1])])"
 }
+solution("123456789")
+
